1fe90b88631858e1e70463d232b29170b539e98a
jagdeesh kushwah <jag84960@gmail.com>
2025-04-25 20:55:10 +0530

    sixth

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+var menu = document.querySelector(".menu");
+var sidebar = document.querySelector(".sidebar");
+var upbtn = document.querySelector(".upbtn");
 
-var menu=document.querySelector('.menu')
-var sidebar=document.querySelector('.sidebar')
+menu.addEventListener("click", () => {
+  if (sidebar.style.display === "none") {
+    sidebar.style.display = "flex";
+  } else {
+    sidebar.style.display = "none";
+  }
+});
 
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
 
-
-menu.addEventListener('click',()=>{
-    if(sidebar.style.display==='none'){
-        sidebar.style.display="flex"
-
-    }
-    else{
-        sidebar.style.display='none'
-    }
-})
+upbtn.addEventListener("click", () => {
+  scrollToTop();
+});
